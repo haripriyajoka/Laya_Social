896e4c776ac5ccc4dbda5398346f0b4c72729dc9
hr@redbench.us <megan@ensarsolutions.com>
2023-03-02 19:12:17 +0530

    I

--- 02_index.js
+++ 02_index.js
@@ -1,9 +1,20 @@
 //sidebar//
 const menuItems = document.querySelectorAll('.menu-items');
+
+//-----------------messages-------------//
 const messagesNotification = document.querySelector('#message-notifications');
 const messages = document.querySelector('.messages');
 const message = messages.querySelectorAll('.message');
 const meassageSearch = document .querySelector('#message-search');
+
+//-----------------Theme-------------//
+const theme = document.querySelector ('#Theme');
+const themeModel = document.querySelector ('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span ');
+var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+
+//-----------------remove active class from all menu-items-------------//
 
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -25,6 +36,7 @@ menuItems.forEach(item => {
     })
 })
 
+//----Messages -- searches chat----//
 const searchMessage = () => {
     const val = meassageSearch.value.toLowerCase ();
     message.forEach(user => {
@@ -39,10 +51,89 @@ const searchMessage = () => {
 
 meassageSearch.addEventListener('keyup', searchMessage);
 
+//-------highlights messages card-------//
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = ' 0 0 1rem var(--color-primary)';
     messagesNotification.querySelector('.notification-count').style.display = 'none';
     setTimeout( () => {
         messages.style.boxShadow = 'none';
     }, 2000);
+})
+
+//---theme-- display-customization------//
+const openThemeModel = () => {
+    themeModel.style.display = 'grid';
+}
+const closeThemeModel = (e) => {
+    if(e.target.classList.contains('customize-theme')) {
+        themeModel.style.display = 'none';
+    }
+}
+themeModel.addEventListener('click', closeThemeModel);
+
+theme.addEventListener('click', openThemeModel);
+
+//-------Font-------//
+fontSizes.forEach(size => {
+    let fontSize;
+    size.addEventListener('click', () => {
+        if(size.classList.contains('.font_size-1')){
+            fontSize = '10px';
+            root.style.setProperty('--sticky-top-left','5.4rem');
+            root.style.setProperty('--sticky-top-right','5.4rem');
+        }
+        else if(size.classList.contains('font_size-2')){
+            fontSize = '13px';
+            root.style.setProperty('--sticky-top-left','5.4rem');
+            root.style.setProperty('--sticky-top-right','-7rem');
+        }
+        else if(size.classList.contains('font_size-3')){
+            fontSize = '16px';
+            root.style.setProperty('--sticky-top-left','-2rem');
+            root.style.setProperty('--sticky-top-right','-17rem');
+        }
+        else if(size.classList.contains('font_size-4')){
+            fontSize = '19px';
+            root.style.setProperty('----sticky-top-left','-5rem');
+            root.style.setProperty('----sticky-top-right','-25rem');
+        }
+        else if(size.classList.contains('font_size-5')){
+            fontSize = '22px';
+            root.style.setProperty('----sticky-top-left','-12rem');
+            root.style.setProperty('----sticky-top-right','-35rem');
+        }
+    //---change font size of the root html element---//
+        document.querySelector('html').style.fontSize = fontSize;
+    })
+})
+//---reove active class from color----//
+const changeAvtiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+//----- change primary color----//
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        let primary;
+        changeAvtiveColorClass ();
+        if(color.classList.contains('color-1')) {
+            primaryHue = 252;
+        }
+        else if (color.classList.contains('color-2')) {
+            primaryHue = 52;
+        }
+        else if (color.classList.contains('color-3')) {
+            primaryHue = 352;
+        }
+        else if (color.classList.contains('color-4')) {
+            primaryHue = 152;
+        }
+        else if (color.classList.contains('color-5')) {
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+        root.style.setProperty('--primary-color-hue', primaryHue);
+    })
 })
